c9dcbf0e98e04389ebe02ddd2b33a742a7844988
stylescube <sdastouri@gmail.com>
2019-01-30 12:12:12 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $dbhost = '127.0.0.1:49944';
-    $dbuser = 'user';
-    $dbpass = 'pass';
+    $dbuser = 'azure';
+    $dbpass = '6#vWHD_$';
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass, 'localdb');
     if(mysqli_connect_errno()) {
         die('Could not connect: ' . mysqli_error($conn));
